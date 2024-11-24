7396772377a0e6cfa0eaab4cc93281dd9d3ef10d
Takaki Hiko <1943767335@qq.com>
2024-11-24 14:00:29 +0800

    construct

--- codes/proj-01-heat1d/err.m
+++ codes/proj-01-heat1d/err.m
@@ -1,4 +1,28 @@
-ux1=1;
-u0=0;
-f=
+clear;
+clc;
+ux1=0;
+u0=1;
+u=@(x) x.^5;
+calu=1/11; %u平方在0到1的积分
+calux=16/9;%u对x的偏导的平方在0到1的积分值
+max_point=16;
+erro=zeros(max_point/2,2);%create an arrary to storage error
+
+for i=2:2:16
+pp=1;
+n_el=i;
+n_int=10;
+uh= driver(pp,n_el,n_int,ux1,u0);
+h=1/n_el/pp;%Determine the interval between each adjacent point
+x=0:h:1-h;
+ur=u(x);
+
+for j=1:i
+%     erro(i/2,1)=erro(i/2,1)+;
+end
+end
+
+
+
+
 
