172e9409ac72ecafde9ff67af95b605cc6bd7b12
Takaki Hiko <1943767335@qq.com>
2024-11-25 15:41:43 +0800

    calculate error of u

--- codes/proj-01-heat1d/err.m
+++ codes/proj-01-heat1d/err.m
@@ -17,12 +17,20 @@ h=1/n_el/pp;%Determine the interval between each adjacent point
 x=0:h:1-h;
 ur=u(x);
 
-for j=1:i
-%     erro(i/2,1)=erro(i/2,1)+;
+for j=1:i-1
+    x1=x(1,j);
+    x2=x(1,j+1);
+    y1=uh(j,1);
+    y2=uh(j+1,1);
+    p=(y2-y1)/(x2-x1);
+erro(i/2,1)=erro(i/2,1)+(cha(x1,x2,11)-2*p*(cha(x1,x2,7)-x1*cha(x1,x2,6))+y1*cha(x1,x2,6)+p^2....
+*(cha(x1,x2,3)-x1*cha(x1,x2,2)+cha(x1,x2,1)*x1^2)+y1^2*cha(x1,x2,1)-2*p*y1*(cha(x1,x2,2)-x1*cha(x1,x2,1)))/calu;
 end
 end
 
 
-
+function [result]= cha(a,b,t)
+result=(b.^t-a.^t)./t;
+end
 
 
