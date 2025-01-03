63010fe5cc95e45f56ca39ea6e29d6ece2e55e65
Takaki Hiko <1943767335@qq.com>
2025-01-04 00:05:17 +0800

    read msh file

--- FINAL/readmsh.m
+++ FINAL/readmsh.m
@@ -1,35 +1,36 @@
-function [nodes, elements] = readmsh(filename)
-    % 打开文件
-    fid = fopen(filename, 'r');
-    if fid == -1
-        error('无法打开文件');
-    end
+filename = 'mesh.txt'; % 替换为实际文件名
+fileID = fopen(filename, 'r');
 
-    % 初始化变量
-    nodes = [];
-    elements = [];
+% 初始化存储
+nodes = [];
+elements = [];
 
-    while ~feof(fid)
-        line = fgetl(fid);
-        
-        if contains(line, '$Nodes')
-            % 读取节点数据
-            numNodes = str2double(fgetl(fid));  % 节点数目
-            nodes = zeros(numNodes, 4);
-            for i = 1:numNodes
-                nodes(i, :) = fscanf(fid, '%f', 4);
-            end
-        elseif contains(line, '$Elements')
-            % 读取元素数据
-            numElements = str2double(fgetl(fid));  % 元素数目
-            elements = zeros(numElements, 5);  % 假设每个元素有5个数据项
-            for i = 1:numElements
-                elements(i, :) = fscanf(fid, '%f', 5);
-            end
+% 逐行读取文件
+while ~feof(fileID)
+    line = fgetl(fileID);
+    if contains(line, '$ParametricNodes')
+        % 读取节点部分
+        numNodes = str2double(fgetl(fileID));
+        nodes = zeros(numNodes, 4); % 假设节点有4列数据
+        for i = 1:numNodes
+            data = sscanf(fgetl(fileID), '%f')';
+            nodes(i, :) = data(1:4); % 取前4列
+        end
+    elseif contains(line, '$Elements')
+        % 读取单元部分
+        numElements = str2double(fgetl(fileID));
+        elements = cell(numElements, 1);
+        for i = 1:numElements
+            data = sscanf(fgetl(fileID), '%f')';
+            elements{i} = data; % 保存整个单元定义
         end
     end
-    
-    % 关闭文件
-    fclose(fid);
 end
 
+fclose(fileID);
+
+% 输出结果
+disp('Nodes:');
+disp(nodes);
+disp('Elements:');
+disp(elements);
