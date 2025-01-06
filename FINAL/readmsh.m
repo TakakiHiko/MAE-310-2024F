04486497087f99be489d72030df8d5b2a9aa882a
Takaki Hiko <1943767335@qq.com>
2025-01-07 00:41:03 +0800

    GENERAte coordinate

--- FINAL/readmsh.m
+++ FINAL/readmsh.m
@@ -1,6 +1,7 @@
 filename = 'mesh.txt';
 fileID = fopen(filename, 'r');
-
+po=0.3;%泊松比
+ym=1e9;%杨氏模量
 % 初始化存储
 nodes = [];
 elements = [];
@@ -26,18 +27,18 @@ while ~feof(fileID)
             elements{i} = data; % 保存整个单元定义
         end
     end
+
 end
 
-fclose(fileID);
 IEN=[];
 lineflag=1;
 for i=1:numElements
 vec=cell2mat(elements(i,1));
 if vec(1,2)==3
 tips=vec(1,3);
-nodes=vec(1,4+tips:length(vec));
+nodess=vec(1,4+tips:length(vec));
 IEN(lineflag,1)=lineflag;
-IEN(lineflag,2:1+length(nodes))= nodes;
+IEN(lineflag,2:1+length(nodess))= nodess;
 lineflag=lineflag+1;
 end
 end
@@ -48,29 +49,8 @@ freedom=2;%节点自由度
 
 ID=generate_ID_from_IEN(IEN,freedom);
 
-function ID = generate_ID_from_IEN(IEN, n_dof_per_node)
-    % 输入:
-    %   IEN            - 节点连接矩阵 (n_element x n_node_per_element)
-    %   n_dof_per_node - 每个节点的自由度数量
-    %
-    % 输出:
-    %   ID - 自由度编号矩阵 (n_element x (n_node_per_element * n_dof_per_node))
-
-    [n_element, n_node_per_element] = size(IEN);
-    ID = zeros(n_element, n_node_per_element * n_dof_per_node);
-
-    for i = 1:n_element
-        for j = 1:n_node_per_element
-            % 当前单元的第 j 个节点
-            global_node = IEN(i, j);
-            for k = 1:n_dof_per_node
-                % 对应的全局自由度
-                global_dof = (global_node - 1) * n_dof_per_node + k;
-                % 填入 ID 矩阵
-                ID(i, (j - 1) * n_dof_per_node + k) = global_dof;
-            end
-        end
-    end
-end
+fclose(fileID);
 
 
+
+    
